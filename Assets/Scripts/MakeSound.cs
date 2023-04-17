9a44afa920f6ebc60b6f21e38f0bc0445b5747dd
balazsivanyi <blzs.ivanyi@gmail.com>
2023-04-17 10:30:43 +0200

    DONE — audio effects are fully responsive and working, damn son

--- Assets/Scripts/MakeSound.cs
+++ Assets/Scripts/MakeSound.cs
@@ -34,30 +34,8 @@ public class MakeSound : MonoBehaviour
             global float myReverbGain2;
             global float myFilterFreq;
             global float myFilterRes;
-
-            2::second => dur beat;
-            SinOsc osc => ADSR env1 => NRev rev1 => dac;
-            (beat / 2, beat / 2, 0, 1::ms) => env1.set;
-            0.2 => osc.gain;
-
-            myReverbGain1 => rev1.mix;
-
-            SinOsc osc2 => ADSR env2 => LPF filter => NRev rev2 => dac;
-            env2 => Delay delay2 => dac;
-            delay2 => delay2;
-
-            (4::ms, beat / 8, 0, 1::ms) => env2.set;
-            0.4 => osc2.gain;
-
-            beat => delay2.max;
-            beat / 8 => delay2.delay;
-            myDelayGain => delay2.gain;
-
-            myReverbGain2 => rev2.mix;
-
-            10000 => filter.freq;
-            10 => filter.Q;
-
+            
+            global Event playSound;
 
             [0, 4, 7, 12] @=> int major[];
             [0, 3, 7, 12] @=> int minor[];
@@ -67,6 +45,29 @@ public class MakeSound : MonoBehaviour
 
             fun void playTwoBars(int position, int chord[]) 
             {    
+                1::second => dur beat;
+                SinOsc osc => ADSR env1 => NRev rev1 => dac;
+                (beat / 2, beat / 2, 0, 1::ms) => env1.set;
+                0.2 => osc.gain;
+
+                myReverbGain1 => rev1.mix;
+
+                SinOsc osc2 => ADSR env2 => LPF filter => NRev rev2 => dac;
+                env2 => Delay delay2 => dac;
+                delay2 => delay2;
+
+                (4::ms, beat / 8, 0, 1::ms) => env2.set;
+                0.4 => osc2.gain;
+
+                beat => delay2.max;
+                beat / 8 => delay2.delay;
+                myDelayGain => delay2.gain;
+
+                myReverbGain2 => rev2.mix;
+
+                myFilterFreq => filter.freq;
+                myFilterRes => filter.Q;
+
                 for(0 => int i; i < 4; i++) 
                 {
                     Std.mtof(chord[0] + offset + position) => osc.freq;
@@ -92,9 +93,9 @@ public class MakeSound : MonoBehaviour
                     playTwoBars(-5, major);   
                 }
             }
-            global Event playSound;
             
-            chout <= ""Rate is "" <= delay2.gain() <= IO.newline();
+            
+            chout <= ""Rate is "" <= myDelayGain <= IO.newline();
             
             while(true) {
                 playSound => now;
@@ -108,14 +109,14 @@ public class MakeSound : MonoBehaviour
         backGround = GameObject.Find("SafeArea").GetComponent<Image>();
 
         // create a callback to use with GetFloar in Update()
-        //myFloatSyncer = gameObject.AddComponent<ChuckFloatSyncer>();
+        myFloatSyncer = gameObject.AddComponent<ChuckFloatSyncer>();
 
         // start syncing effect parameters
-        /* myFloatSyncer.SyncFloat(myChuck, "myDelayGain"); //delay gain
+        myFloatSyncer.SyncFloat(myChuck, "myDelayGain"); //delay gain
         myFloatSyncer.SyncFloat(myChuck, "myReverbGain1"); //reverb1 gain
         myFloatSyncer.SyncFloat(myChuck, "myReverbGain2"); //reverb2 gain
         myFloatSyncer.SyncFloat(myChuck, "myFilterFreq"); //filter freq
-        myFloatSyncer.SyncFloat(myChuck, "myFilterRes"); //filter resonance */
+        myFloatSyncer.SyncFloat(myChuck, "myFilterRes"); //filter resonance
         
     }
 
@@ -152,17 +153,31 @@ public class MakeSound : MonoBehaviour
         float x_axis = Mathf.Clamp01(smoothedAccelerometerData.x + 0.5f);
         float y_axis = Mathf.Clamp01(smoothedAccelerometerData.y + 0.5f);
         float z_axis = Mathf.Clamp01(smoothedAccelerometerData.z + 0.5f);
+        //Debug.Log("x-value: " + x_axis + "y-value: "+ y_axis + "z-value: " + z_axis);
 
         // Set object color to bg
         backGround.material.color = new Color(x_axis, y_axis, z_axis);
-        //Debug.Log("Accelerometer Data: X = " + smoothedAccelerometerData.x + ", Y = " + smoothedAccelerometerData.y + ", Z = " + smoothedAccelerometerData.z);
 
         //remap values to handpicked effect parameters
-        float delayData = Mathf.Clamp(x_axis, 0.0f, 0.7f);
-        Debug.Log(delayData);
+        float delayData = Remap(y_axis, 0.0f, 1.0f, 0.0f, 0.7f);
+        float reverbData1 = Remap(z_axis, 0.0f, 1.0f, 0.0f, 0.2f);
+        float reverbData2 = Remap(z_axis, 0.0f, 1.0f, 0.0f, 0.4f);
+        float filterData1 = Remap(x_axis, 0.0f, 1.0f, 10.0f, 10000.0f);
+        float filterData2 = Remap(x_axis, 0.0f, 1.0f, 0.0f, 30.0f);
+        Debug.Log("delay =" + delayData + "reverb1 =" + reverbData1 + "reverb2 =" + reverbData2 + "filter1 =" + filterData1 + "filter2 =" + filterData2);
 
         //send values to chuck
-        myChuck.SetFloat("myDelayGain", delayData);
+        myFloatSyncer.SetNewValue(delayData);
+        myChuck.SetFloat("myReverbGain1", reverbData1);
+        myChuck.SetFloat("myReverbGain2", reverbData2);
+        myChuck.SetFloat("myFilterFreq", filterData1);
+        myChuck.SetFloat("myFilterRes", filterData2);
+    }
+
+    private float Remap(float aValue, float aIn1, float aIn2, float aOut1, float aOut2)
+    {
+        float t = (aValue - aIn1) / (aIn2 - aIn1);
+        return aOut1 + (aOut2 - aOut1) * t;
     }
 
         
